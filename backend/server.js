4be5d355a23ac151457ea9b58d855a465ec7b1d3
Upendhra Mutyala <143545224+Upendhra777@users.noreply.github.com>
2026-02-23 21:24:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,4 +24,4 @@ app.use("/resume", resumeRoutes);
 app.get("/", (req, res) => res.send("API Running"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT);
